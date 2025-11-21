67be5b9b05113078288576c8737c5e441bf055d3
Zay4r <zayarkasparov@gmail.com>
2025-11-21 20:00:05 +0700

    Update index.js

--- src/problem4/index.js
+++ src/problem4/index.js
@@ -9,3 +9,18 @@ function sum_to_n_iteration(n) {
 }
 
 console.log(sum_to_n_iteration(10));//55
+
+
+//recursive
+//looks better, but will face slow when n is high too
+//lets say n = 10
+function sum_to_n_recursive(n) {
+
+    //base case
+    //here n is still 10, not yet break point
+    if (n <= 1) return n; // break point
+
+    //here n is 10 + ( 9 + ( 8 + ( 7 + ( 6 + ( 5 + ( 4 + ( 3 + ( 2 + (1) ) ) ) ) ) ) ) )
+    return n + sum_to_n_recursive(n - 1); //55
+}
+
